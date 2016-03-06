0a0c3ac06dc82ba23eacac1d66c96efa9c14870b
Martin Hals <martinhals@Martins-MacBook-Pro.local>
2016-03-06 12:43:56 -0800

    Possible to specify through terminal boardsize and color of human and COM

--- reversi.py
+++ reversi.py
@@ -5,16 +5,12 @@ import copy
 BLACK = 1
 WHITE = 2
 
-
 class Board:
 	BLACK = 1
 	WHITE = 2
 	boardsize = None
 	gameboard = None
 	last_move = None
-	# score_white = None
-	# score_black = None
-
 
 	def __init__(self, boardsize):
 		self.boardsize = boardsize
@@ -23,16 +19,6 @@ class Board:
 		self.gameboard[int(boardsize/2)][int(boardsize/2)] = WHITE
 		self.gameboard[int(boardsize/2)][int(boardsize/2) - 1] = BLACK
 		self.gameboard[int(boardsize/2) - 1][int(boardsize/2)] = BLACK
-
-		# self.score_white = self.getScore(WHITE)
-		# self.score_black = self.getScore(BLACK)
-		#testing
-		# self.gameboard[2][1] = WHITE
-		# self.gameboard[0][0] = WHITE	
-		# self.gameboard[1][0] = WHITE
-		# self.gameboard[2][0] = WHITE
-		# self.gameboard[4][4] = BLACK	
-		# self.gameboard[4][5] = BLACK
 
 	def printBoard(self):
 		sys.stdout.write("  ")
@@ -88,7 +74,6 @@ class Board:
 				if(self.gameboard[row][col-i] == my_color):
 					validity = True
 					directions.append('west')
-					# print 'left'
 					break
 		#Right
 		if(col+2 <= self.boardsize-1 and self.gameboard[row][col+1] == opponent_color):
@@ -98,7 +83,6 @@ class Board:
 				if(self.gameboard[row][col+i] == my_color):
 					validity = True
 					directions.append('east')
-					# print 'right'
 					break
 		#Up
 		if(row-2 >= 0 and self.gameboard[row-1][col] == opponent_color):
@@ -108,7 +92,6 @@ class Board:
 				if(self.gameboard[row-i][col] == my_color):
 					validity = True
 					directions.append('north')
-					# print 'up'
 					break
 		#Down
 		if(row+2 <= self.boardsize-1 and self.gameboard[row+1][col] == opponent_color):
@@ -119,7 +102,6 @@ class Board:
 				if(self.gameboard[row+i][col] == my_color):
 					validity = True
 					directions.append('south')
-					# print 'down'
 					break
 		#Diagonally up to the left
 		if(row-2 >= 0 and col-2 >= 0 and self.gameboard[row-1][col-1] == opponent_color):
@@ -129,7 +111,6 @@ class Board:
 				if(self.gameboard[row-i][col-i] == my_color):
 					validity = True
 					directions.append('northwest')
-					# print 'diag up left'
 					break
 		#Diagonally up to the right
 		if(row-2 >= 0 and col+2 <= self.boardsize-1 and self.gameboard[row-1][col+1] == opponent_color):
@@ -139,7 +120,6 @@ class Board:
 				if(self.gameboard[row-i][col+i] == my_color):
 					validity = True
 					directions.append('northeast')
-					# print 'diag up right'
 					break
 		#Diagonally down to the left
 		if(row+2 <= self.boardsize-1 and col-2 >= 0 and self.gameboard[row+1][col-1] == opponent_color):
@@ -149,7 +129,6 @@ class Board:
 				if(self.gameboard[row+i][col-i] == my_color):
 					validity = True
 					directions.append('southwest')
-					# print 'diag down left'
 					break
 		#Diagonally down to the right
 		if(row+2 <= self.boardsize-1 and col+2 <= self.boardsize-1 and self.gameboard[row+1][col+1] == opponent_color):
@@ -159,7 +138,6 @@ class Board:
 				if(self.gameboard[row+i][col+i] == my_color):
 					validity = True
 					directions.append('southeast')
-					# print 'diag down right'
 					break
 
 		return validity, directions
@@ -229,17 +207,8 @@ class Board:
 		return valid
 
 	def printScore(self):
-		# score_white = 0
-		# score_black = 0
-		# for row in range(0,self.boardsize-1):
-		# 	for col in range(0, self.boardsize-1):
-		# 		if(self.gameboard[row][col] == 1):
-		# 			score_black += 1
-		# 		if(self.gameboard[row][col] == 2):
-		# 			score_white += 1
 		score_white = self.getScore(WHITE)
 		score_black = self.getScore(BLACK)
-
 		sys.stdout.write('Score light: %2d'   % score_white)
 		sys.stdout.write('        Score dark: %2d \n' % score_black)
 
@@ -348,11 +317,14 @@ def aiVSai(boardsize):
 		#AI1 make move
 		if(board.checkIfAnyValidMoves(WHITE) == True):
 			move_ai = ai1.makeMoveAI()
-			row = move_ai[0]
-			col = move_ai[1]
-			board.makeMove(col,row,WHITE)
+			row_int = move_ai[0]
+			col_int = move_ai[1]
+			board.makeMove(col_int,row_int,WHITE)
+			row = str(unichr(row_int + 49))
+			col = str(unichr(col_int + 97))
+			move = col + row
 			board.printBoard()
-			print 'LIGHT ', move_ai 
+			print 'LIGHT ', move
 			board.printScore()
 			if(board.gameOver() == True):
 				break
@@ -360,11 +332,14 @@ def aiVSai(boardsize):
 		#AI2 make move
 		if(board.checkIfAnyValidMoves(BLACK) == True):
 			move_ai = ai2.makeMoveAI()
-			row = move_ai[0]
-			col = move_ai[1]
-			board.makeMove(col,row,BLACK)
+			row_int = move_ai[0]
+			col_int = move_ai[1]
+			board.makeMove(col_int,row_int,BLACK)
+			row = str(unichr(row_int + 49))
+			col = str(unichr(col_int + 97))
+			move = col + row
 			board.printBoard()
-			print 'DARK ', move_ai 
+			print 'DARK ', move
 			board.printScore()
 			if(board.gameOver() == True):
 				break
@@ -377,48 +352,85 @@ def aiVSai(boardsize):
 	print 'Game over! The winner is:', winner
 
 
-def newGame(boardsize):
-	color_human = WHITE
-	color_ai = BLACK
+def newGame(boardsize, color_human):
+	if(color_human == BLACK):
+		color_ai = WHITE
+	else:
+		color_ai = BLACK
+
 	depth = 4
 	board = Board(boardsize)
 	ai = Ai(board, color_ai, depth)
 	board.printBoard()
 
-	# valid_moves = board.getValidMoves(color_ai)
-	# for move in valid_moves:
-	# 	move.printBoard()
-
-	while(True):
-		#Player make move
-		if(board.checkIfAnyValidMoves(color_human) == True):
-			while(True):
-				move_human = raw_input('Make a move: ')
-				move = list(move_human)
-				col = ord(move[0]) - ord('a')
-				row = ord(move[1]) - ord('1')
-				valid = board.makeMove(col,row,color_human)
-				if(valid == True):
+	if(color_human == BLACK): #Human plays first
+		while(True):
+			#Player make move
+			if(board.checkIfAnyValidMoves(color_human) == True):
+				while(True):
+					move_human = raw_input('Make a move: ')
+					move = list(move_human)
+					col = ord(move[0]) - ord('a')
+					row = ord(move[1]) - ord('1')
+					valid = board.makeMove(col,row,color_human)
+					if(valid == True):
+						break
+				board.printBoard()
+				board.printScore()
+				if(board.gameOver() == True):
 					break
-			board.printBoard()
-			board.printScore()
-			if(board.gameOver() == True):
-				break
 
-		#AI make move
-		if(board.checkIfAnyValidMoves(color_ai) == True):
-			move_ai = ai.makeMoveAI()
-			row = move_ai[0]
-			col = move_ai[1]
-			board.makeMove(col,row,color_ai)
-			board.printBoard()
-			print 'AI moves ', move_ai 
-			board.printScore()
-			if(board.gameOver() == True):
-				break
+			#AI make move
+			if(board.checkIfAnyValidMoves(color_ai) == True):
+				move_ai = ai.makeMoveAI()
+				row_int = move_ai[0]
+				col_int = move_ai[1]
+				board.makeMove(col_int,row_int,color_ai)
+				row = str(unichr(row_int + 49))
+				col = str(unichr(col_int + 97))
+				move = col + row
+				board.printBoard()
+				print 'AI moves ', move 
+				board.printScore()
+				if(board.gameOver() == True):
+					break
+
+	else:	#AI plays first
+		while(True):
+			#AI make move
+			if(board.checkIfAnyValidMoves(color_ai) == True):
+				move_ai = ai.makeMoveAI()
+				row_int = move_ai[0]
+				col_int = move_ai[1]
+				board.makeMove(col_int,row_int,color_ai)
+				row = str(unichr(row_int + 49))
+				col = str(unichr(col_int + 97))
+				move = col + row
+				board.printBoard()
+				print 'AI moves ', move
+				board.printScore()
+				if(board.gameOver() == True):
+					break
+			#Player make move
+			if(board.checkIfAnyValidMoves(color_human) == True):
+				while(True):
+					move_human = raw_input('Make a move: ')
+					move = list(move_human)
+					col = ord(move[0]) - ord('a')
+					row = ord(move[1]) - ord('1')
+					valid = board.makeMove(col,row,color_human)
+					if(valid == True):
+						break
+				board.printBoard()
+				board.printScore()
+				if(board.gameOver() == True):
+					break
+
 
 	if(board.getScore(color_human) > board.getScore(color_ai)):
 		winner = 'human'
+	elif(board.getScore(color_human) == board.getScore(color_ai)):
+		winner = 'It is actually a tie!'
 	else:
 		winner = 'AI'
 
@@ -430,9 +442,22 @@ def newGame(boardsize):
 
 
 def main():
-	boardsize = 8
-	# newGame(boardsize)
-	aiVSai(boardsize)
+	boardsize = 0
+	color_human = BLACK
+	for i in range(0,len(sys.argv)):
+		if(sys.argv[i] == '-n'):
+			boardsize = ord(sys.argv[i+1]) - ord('1') + 1
+		if(sys.argv[i] == '-l'): #Human is going to play with light pieces
+			color_human = WHITE
+
+	if(boardsize == 0):
+		boardsize = 8	#Custom board size if nothing else specified
+
+	print 'Board size =', boardsize
+	print 'Human color =', color_human
+
+	newGame(boardsize, color_human)
+	# aiVSai(boardsize)
 
 
 main()
